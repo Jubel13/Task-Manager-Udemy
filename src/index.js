6d048cd71cd5053ba494c98a9a90c009c5933563
Jubel13 <jubelsinaga13@gmail.com>
2021-11-29 08:54:20 +0700

    Add post route to task

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.post("/users", (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch((error) => {
       res.status(400).send(error);
@@ -28,7 +28,7 @@ app.post("/tasks", (req, res) => {
   task
     .save()
     .then(() => {
-      res.send(task);
+      res.status(201).send(task);
     })
     .catch((error) => {
       res.status(400).send(error);
